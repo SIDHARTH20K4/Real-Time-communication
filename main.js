e264fef5514c3ba1629044bf3ec50757ed1fe59a
SIDHARTH20K4 <91970588+SIDHARTH20K4@users.noreply.github.com>
2025-02-26 23:43:02 +0530

    learning events and emits

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const { Socket } = require('dgram');
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
 
 io.on('connection', (socket) => {
-    console.log('user connected');
+    console.log('connected to the server');
 
-    socket.on('signal', (data) => {
-        socket.broadcast.emit('signal', data);
-    })
+    socket.on('chat message', (msg) => {
+        console.log('recieved message is: ' + msg);
+        socket.emit('msg', "recieved message from the client");
+    });
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
-    })
+        console.log('Disconnected from the server');
+    });
 })
 
 httpServer.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+    console.log('server is listening');
+})
