4e2136f769ebd8a2e3c730b6064c99d845cea296
SIDHARTH20K4 <91970588+SIDHARTH20K4@users.noreply.github.com>
2025-03-02 23:38:08 +0530

    trying to create a messenger app

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const { Socket } = require('dgram');
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -10,21 +9,33 @@ const io = new Server(httpServer);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'ind.html'));
-})
+});
 
 io.on('connection', (socket) => {
-    console.log('connected to the server');
+    console.log(`Client connected: ${socket.id}`);
+    socket.emit('connected', 'You are connected to the server');
+
+    socket.on('setUsername', (username) => {
+        socket.username = username; // Store username in the socket object
+        console.log(`User ${socket.id} set their username as ${socket.username}`);
+    });
 
     socket.on('chat message', (msg) => {
-        console.log('recieved message is: ' + msg);
-        socket.emit('msg', "recieved message from the client");
+        console.log(`${socket.username || 'Unknown'}: ${msg}`);
+        io.emit('msg', `${socket.username || 'Unknown'}: ${msg}`);
+    });
+
+    socket.on('connected to room', (room) => {
+        socket.join(room);
+        console.log(`Socket ${socket.id} joined room: ${room}`);
     });
 
     socket.on('disconnect', () => {
-        console.log('Disconnected from the server');
+        console.log(`User ${socket.username || 'Unknown'} disconnected`);
+        socket.broadcast.emit('userDisconnected', socket.id, socket.username || 'Unknown');
     });
-})
+});
 
 httpServer.listen(3000, () => {
-    console.log('server is listening');
-})
+    console.log(`Server is listening`);
+});
